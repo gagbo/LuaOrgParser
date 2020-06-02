e36d476b9aeb4efe380f49171d6104dc453c0c83
Gerry Agbobada <gagbobada+git@gmail.com>
2020-06-02 20:49:34 +0200

    Add basic implementation of headline with tests to fix

--- src/luaorgparser/parser.lua
+++ src/luaorgparser/parser.lua
@@ -1,11 +1,54 @@
 -- Parsing functions
 
+local lpeg = require("lpeg")
+local P, R, S, Cg, Ct = lpeg.P, lpeg.R, lpeg.S, lpeg.Cg, lpeg.Ct
+
 local astHeadline = require("luaorgparser/ast/headline")
 
 local parser = {}
 
+-- Parsing helpers
+local whitespace = S("\r\n\f\t ") ^ 1
+local whitespaceOrEndOfInput = whitespace + -1
+
+-- range of valid characters after first character of identifier
+local tagsafe = R("AZ", "az", "09") + S("_", "@", "#", "%")
+
+-- Headline patterns
+local stars = P("*") ^ 1
+-- TODO : affect the correct list of org-todo-keywords here
+local keyword = P("TODO") + P("DONE")
+local priority = P("[#") * R("AZ", "az") * P("]")
+local tags = P(":") * tagsafe * (":" * tagsafe) ^ 0 * P(":")
+local headlinePattern =
+    Ct(
+    (Cg(stars, "stars") * whitespaceOrEndOfInput) * -- Stars followed by whitespace or EOL
+        (Cg(keyword, "keyword") * whitespaceOrEndOfInput) ^ -1 * -- Optional todo keyword
+        (Cg(priority, "priority") * whitespaceOrEndOfInput) ^ -1 * -- Optional Priority
+        (Cg((P(1) - tags) ^ 0, "title") * whitespaceOrEndOfInput) ^ -1 * -- Optional Title
+        (Cg(tags, "tags") * whitespaceOrEndOfInput) ^ -1 -- Optional Tags
+)
+
 function parser.parseHeadline(line)
-    local newHeadline = astHeadline:new()
+    local parsedHeadline = headlinePattern:match(line)
+
+    -- local inspect = require("inspect")
+    -- print(inspect(parsedHeadline))
+
+    if not parsedHeadline then
+        return nil
+    end
+
+    local newHeadline =
+        astHeadline:new(
+        {
+            level = string.len(parsedHeadline.stars),
+            title = parsedHeadline.title,
+            priority = parsedHeadline.priority,
+            todo_keyword = parsedHeadline.keyword,
+            tags = parsedHeadline.tags
+        }
+    )
     return newHeadline
 end
 
--- tests/testing_parser.lua
+++ tests/testing_parser.lua
@@ -13,6 +13,87 @@ function TestParser:testBasicHeadline()
     local result = parser.parseHeadline(headline)
     lu.assertEquals(result.node_type, "headline")
     lu.assertEquals(result.level, 1)
+    lu.assertEquals(result.title, "Level 1")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, nil)
+end
+
+function TestParser:testDeepLevelHeadline()
+    local headline = "***** Level 5"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 5)
+    lu.assertEquals(result.title, "Level 5")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, nil)
+end
+
+function TestParser:testSingleWordHeadline()
+    local headline = "** Single"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 2)
+    lu.assertEquals(result.title, "Single")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, nil)
+end
+
+function TestParser:testSingleTODOHeadline()
+    local headline = "*** TODO This is a todo"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 3)
+    lu.assertEquals(result.title, "This is a todo")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, "TODO")
+    lu.assertEquals(result.tags, nil)
+end
+
+function TestParser:testSinglePriorityHeadline()
+    local headline = "**** [#A] This is an important item"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 4)
+    lu.assertEquals(result.title, "This is an important item")
+    lu.assertEquals(result.priority, "[#A]")
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, nil)
+end
+
+function TestParser:testSingleTagHeadline()
+    local headline = "****** One tag :test:"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 6)
+    lu.assertEquals(result.title, "One tag")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, {"test"})
+end
+
+function TestParser:testEmptyHeadline()
+    local headline = "*******"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 7)
+    lu.assertEquals(result.title, "")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, nil)
+end
+
+function TestParser:testOnlyTagsHeadline()
+    local headline = "******** :ARCHIVE:%Paris:"
+    local result = parser.parseHeadline(headline)
+    lu.assertEquals(result.node_type, "headline")
+    lu.assertEquals(result.level, 8)
+    lu.assertEquals(result.title, "")
+    lu.assertEquals(result.priority, nil)
+    lu.assertEquals(result.todo_keyword, nil)
+    lu.assertEquals(result.tags, {"ARCHIVE", "%Paris"})
 end
 
 os.exit(lu.LuaUnit.run())
